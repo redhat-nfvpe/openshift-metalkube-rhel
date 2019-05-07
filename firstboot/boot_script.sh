546432d5211357018a47d9ed718c1cd00bc59cc1
Yolanda Robla <yroblamo@redhat.com>
2019-05-07 14:59:22 +0200

    Add changes in boot script

--- firstboot/boot_script.sh
+++ firstboot/boot_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat > /etc/yum.repos.d/redhat.repo <<EOF
+cat > /etc/yum.repos.d/rhel.repo <<EOF
 [rhel-8-for-x86_64-baseos-beta-rpms]
 name = Red Hat Enterprise Linux 8 for x86_64 - BaseOS Beta (RPMs)
 baseurl = https://downloads.redhat.com/redhat/rhel/rhel-8-beta/baseos/x86_64/
@@ -16,4 +16,15 @@ gpgcheck = 1
 gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-beta,file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
 EOF
 
-yum update
+dnf -y update
+dnf install python3 --allowerasing -y
+dnf install -y git
+
+pip3 install ansible
+ln -sf /usr/bin/python3 /usr/bin/python
+
+pushd tmp
+git clone https://github.com/yrobla/openshift-metalkube-rhel
+pushd openshift-metalkube-rhel/ansible
+
+ansible-playbook play.yml -i localhost,
