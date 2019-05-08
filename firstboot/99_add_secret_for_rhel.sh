46cff28ec883ee11e709c8be18c88ad5bef859eb
Yolanda Robla <yroblamo@redhat.com>
2019-05-08 10:14:38 +0200

    Add script to generate secret for worker

---
+++ firstboot/99_add_secret_for_rhel.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source $HOME/settings.env
+
+IGNITION_ENDPOINT="https://api.${CLUSTER_NAME}.${BASE_DOMAIN}:22623/config/worker"
+
+cat > .cloudinit.tmp << EOF
+write_files:
+- path: /tmp/pull.json
+  content: |
+    $PULL_SECRET
+- path: /root/.kube/config
+  encoding: b64
+  content: $(cat ocp/auth/kubeconfig | base64)
+- path: /tmp/ignition_endpoint
+  content: $IGNITION_ENDPOINT
+
+users:
+  - name: core
+    groups: wheel
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    ssh_authorized_keys:
+      - $(cat $HOME/.ssh/id_rsa.pub)
+
+rh_subscription:
+  username: $RH_USERNAME
+  password: '$RH_PASSWORD'
+  auto-attach: True
+EOF
+
+cat << EOF
+apiVersion: v1
+data:
+  userData: $(base64 -w 0 .cloudinit.tmp)
+kind: Secret
+metadata:
+  name: rhel-worker-user-data
+  namespace: metal3
+type: Opaque
+EOF
+
