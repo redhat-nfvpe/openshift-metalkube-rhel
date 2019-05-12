2f96bccbc8508b80cf9a5a995ab381a063b27329
Yolanda Robla <yroblamo@redhat.com>
2019-05-12 09:02:48 +0200

    Isolate script to enroll a rhel node

--- scripts/99_add_secret_for_rhel.sh
+++ scripts/99_add_secret_for_rhel.sh
@@ -12,33 +12,12 @@ write_files:
     encoding: b64
 -   path: /root/.kube/config
     encoding: b64
-    content: $(cat ocp/auth/kubeconfig | base64 | tr -d '\n')
+    content: $(cat $KUBECONFIG_PATH | base64 | tr -d '\n')
 -   path: /tmp/ignition_endpoint
     content: $IGNITION_ENDPOINT
--   path: /tmp/cloud_init_script.sh
-    content: |
-        #!/bin/bash
-        set -eux
-        dhclient eth1
-        subscription-manager register --username $RH_USERNAME --password $RH_PASSWORD --force
-        subscription-manager attach --pool=$RH_POOL
-        subscription-manager repos --enable=rhel-7-server-rpms
-        subscription-manager repos --enable=rhel-7-server-extras-rpms
-        subscription-manager repos --enable=rhel-7-server-rh-common-rpms
-        subscription-manager repos --enable=rhel-7-server-rt-rpms
-        yum update -y
-        yum -y -t install git epel-release python-setuptools wget
-        yum -y groupinstall RT
-        pushd /tmp
-        wget https://bootstrap.pypa.io/get-pip.py
-        python get-pip.py
-        pip install ansible
-        rm -rf openshift-metalkube-rhel
-        git clone https://github.com/yrobla/openshift-metalkube-rhel
-        pushd openshift-metalkube-rhel/ansible
-        ansible-playbook -v play.yml -i localhost,
-        sed -i '/^.*linux16.*/ s/$/ ip=eth0:dhcp ip=eth1:dhcp rd.neednet=1/' /boot/grub2/grub.cfg
-        reboot
+-   path: /tmp/enroll_rhel_node.sh
+    encoding: b64
+    content: $(cat scripts/enroll_rhel_node.sh | base64 | tr -d '\n')
 
 users:
   - name: core
@@ -48,7 +27,8 @@ users:
       - $(cat $HOME/.ssh/id_rsa.pub)
 
 runcmd:
- - [ bash, /tmp/cloud_init_script.sh ]
+ - [ bash, /tmp/enroll_rhel_node.sh ]
+ - [ reboot ]
 
 EOF
 
---
+++ scripts/enroll_rhel_node.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -eux
+# give network before starting
+dhclient eth1
+
+# enable subscription
+subscription-manager register --username $RH_USERNAME --password $RH_PASSWORD --force
+subscription-manager attach --pool=$RH_POOL
+subscription-manager repos --enable=rhel-7-server-rpms
+subscription-manager repos --enable=rhel-7-server-extras-rpms
+subscription-manager repos --enable=rhel-7-server-rh-common-rpms
+subscription-manager repos --enable=rhel-7-server-rt-rpms
+
+# install packages
+yum-config-manager --add-repo https://cbs.centos.org/repos/paas7-crio-113-candidate/x86_64/os
+yum-config-manager --add-repo  https://rpms.svc.ci.openshift.org/openshift-origin-v4.0/
+yum update -y
+
+yum -y -t install git epel-release wget kernel irqbalance microcode_ctl systemd selinux-policy-targeted setools-console dracut-network passwd openssh-server openssh-clients podman skopeo runc containernetworking-plugins cri-tools nfs-utils NetworkManager dnsmasq lvm2 iscsi-initiator-utils sg3_utils device-mapper-multipath xfsprogs e2fsprogs mdadm cryptsetup chrony logrotate sssd shadow-utils sudo coreutils less tar xz gzip bzip2 rsync tmux nmap-ncat net-tools bind-utils strace bash-completion vim-minimal nano authconfig iptables-services biosdevname container-storage-setup cloud-utils-growpart glusterfs-fuse cri-o openshift-clients openshift-hyperkube
+
+# enable cri-o
+systemctl start cri-o
+systemctl enable cri-o
+
+# disable swap
+swapoff -a
+
+# enable ip forwarding
+sysctl -w net.ipv4.ip_forward=1
+sysctl --system
+
+# set sebool container_manage_cgroup
+setsebool -P container_manage_cgroup on || true
+
+# create temporary directory and extract contents there
+TEMP_DIR=$(mktemp -d)
+pushd $TEMP_DIR
+IGNITION_URL=$(cat /tmp/ignition_endpoint )
+curl -k $IGNITION_URL -o $TEMP_DIR/bootstrap.ign
+
+# run release image
+CLUSTER_VERSION=$(oc get clusterversion --config=$HOME/.kube/config --output=jsonpath='{.items[0].status.desired.image}')
+podman pull --tls-verify=false --authfile /tmp/pull.json $CLUSTER_VERSION
+RELEASE_IMAGE=$(podman run --rm $CLUSTER_VERSION image machine-config-daemon)
+
+# run MCD image
+podman pull --tls-verify=false --authfile /tmp/pull.json $RELEASE_IMAGE
+podman run -v /:/rootfs -v /var/run/dbus:/var/run/dbus -v /run/systemd:/run/systemd --privileged --rm -ti $RELEASE_IMAGE start --node-name $HOSTNAME --once-from $TEMP_DIR/bootstrap.ign --skip-reboot
+
+sed -i '/^.*linux16.*/ s/$/ ip=eth0:dhcp ip=eth1:dhcp rd.neednet=1/' /boot/grub2/grub.cfg
