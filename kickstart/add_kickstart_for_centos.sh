545d348e497861286c222c029559e8fcac2443c7
Yolanda Robla <yroblamo@redhat.com>
2019-06-07 14:41:44 +0200

    Run post script outside chroot

--- kickstart/add_kickstart_for_centos.sh
+++ kickstart/add_kickstart_for_centos.sh
@@ -4,7 +4,7 @@ source $HOME/settings.env
 
 IGNITION_ENDPOINT="https://api.${CLUSTER_NAME}.${BASE_DOMAIN}:22623/config/worker"
 CORE_SSH_KEY=$(cat $HOME/.ssh/id_rsa.pub)
-ENROLL_CENTOS_NODE=$(cat ../scripts/enroll_centos_node.sh)
+ENROLL_CENTOS_NODE=$(cat ./scripts/enroll_centos_node.sh)
 KUBECONFIG_FILE=$(cat $KUBECONFIG_PATH)
 
 cat > centos-worker-kickstart.cfg <<EOT
@@ -26,44 +26,44 @@ services --enabled=iptables
 skipx
 firstboot --disable
 user --name=core --groups=wheel
-%post --erroronfail --log=/root/ks-post.log
+%post --nochroot --erroronfail --log=/mnt/sysimage/root/ks-post.log
 
 # Add core ssh key
-mkdir -m0700 /home/core/.ssh
-cat <<EOF > /home/core/.ssh/authorized_keys
+mkdir -m0700 /mnt/sysimage/home/core/.ssh
+cat <<EOF > /mnt/sysimage/home/core/.ssh/authorized_keys
 ${CORE_SSH_KEY}
 EOF
-chmod 0600 /home/core/.ssh/authorized_keys
-chown -R core:core /home/core/.ssh
-restorecon -R /home/core/.ssh
+chmod 0600 /mnt/sysimage/home/core/.ssh/authorized_keys
+chown -R core:core /mnt/sysimage/home/core/.ssh
+restorecon -R /mnt/sysimage/home/core/.ssh
 
 # enable passwordless sudo for wheel
-echo "%wheel   ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/wheel
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+echo "%wheel   ALL=(ALL)       NOPASSWD: ALL" >> /mnt/sysimage/etc/sudoers.d/wheel
+sed -i "s/^.*requiretty/#Defaults requiretty/" /mnt/sysimage/etc/sudoers
 
 # write pull secret
-cat <<EOF > /tmp/pull.json
+cat <<EOF > /mnt/sysimage/tmp/pull.json
 ${PULL_SECRET}
 EOF
 
 # write kubeconfig
-mkdir -p /root/.kube
-cat <<EOF > /root/.kube/config
+mkdir -p /mnt/sysimage/root/.kube
+cat <<EOF > /mnt/sysimage/root/.kube/config
 ${KUBECONFIG_FILE}
 EOF
 
 # write ignition endpoint
-cat <<EOF > /tmp/ignition_endpoint
+cat <<EOF > /mnt/sysimage/tmp/ignition_endpoint
 ${IGNITION_ENDPOINT}
 EOF
 
 # write enroll script
-cat <<'EOF' > /tmp/enroll_centos_node.sh
+cat <<'EOF' > /mnt/sysimage/tmp/enroll_centos_node.sh
 ${ENROLL_CENTOS_NODE}
 EOF
 
 # execute enroll script
-bash /tmp/enroll_centos_node.sh
+bash /mnt/sysimage/tmp/enroll_centos_node.sh
 %end
 %packages
 @base
